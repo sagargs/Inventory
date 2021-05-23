8b20017299e1a01cb721263b026de322ddd7bc24
sagargs <67275696+sagargs@users.noreply.github.com>
2021-05-23 13:30:31 +0530

    Add files via upload

---
+++ Products1Controller.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Think_Bridge.Models;
+
+namespace Think_Bridge.Controllers
+{
+    public class Products1Controller : ApiController
+    {
+        private InventoryDBModel db = new InventoryDBModel();
+
+        // GET: api/Products1
+        public async Task<IQueryable<Products>> GetProducts()
+        {
+            await Task.Delay(1);
+            return db.Products;
+        }
+
+        // GET: api/Products1/5
+        [ResponseType(typeof(Products))]
+        public async Task<IHttpActionResult> GetProducts(int id)
+        {
+            Products products = await db.Products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(products);
+        }
+
+        // PUT: api/Products1/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutProducts(int id, Products products)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != products.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(products).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Products1
+        [ResponseType(typeof(Products))]
+        public async Task<IHttpActionResult> PostProducts(Products products)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Products.Add(products);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = products.id }, products);
+        }
+
+        // DELETE: api/Products1/5
+        [ResponseType(typeof(Products))]
+        public async Task<IHttpActionResult> DeleteProducts(int id)
+        {
+            Products products = await db.Products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            db.Products.Remove(products);
+            await db.SaveChangesAsync();
+
+            return Ok(products);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductsExists(int id)
+        {
+            return db.Products.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ ProductsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Think_Bridge.Models;
+
+namespace Think_Bridge.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private InventoryDBModel db = new InventoryDBModel();
+
+        // GET: Products
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Products.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = await db.Products.FindAsync(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,name,description,price")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(products);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(products);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = await db.Products.FindAsync(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,name,description,price")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(products).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(products);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = await db.Products.FindAsync(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Products products = await db.Products.FindAsync(id);
+            db.Products.Remove(products);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
